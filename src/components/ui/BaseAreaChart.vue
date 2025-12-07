130f262f9ae3debac42fd40795f0875a826ef9ac
Rafli Haidar Nashif <rafli.haidar30@gmail.com>
2025-12-07 21:33:14 +0700

    feat : add prediction

--- src/components/ui/BaseAreaChart.vue
+++ src/components/ui/BaseAreaChart.vue
@@ -29,7 +29,7 @@
 
   const fetchPredictionFromFlask = async (sensorData) => {
     try {
-      const response = await fetch(`http://localhost:5000/predict`, {
+      const response = await fetch(`https://model.binatra.id/predict`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(sensorData)
